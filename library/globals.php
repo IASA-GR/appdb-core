46f11b00877ff2136031b2093a3cd4a228070ba1
William Karageorgos <William.Karageorgos@cern.ch>
2016-10-07 15:17:43 +0300

    [PHP] remove obsolete do_http_request (and friends) from global library

--- library/globals.php
+++ library/globals.php
@@ -724,37 +724,6 @@ function isnull($val) {
 	} else return false;
 }
 
-function do_get_request($url, $data, $optional_headers = null)
-{
-    return do_http_request($url, 'GET', $data, $optional_headers);
-}
-
-function do_post_request($url, $data, $optional_headers = null)
-{
-    return do_http_request($url, 'POST', $data, $optional_headers);
-}
-
-function do_http_request($url, $method, $data, $optional_headers = null)
-{
-  $params = array('http' => array(
-              'method' => $method,
-              'content' => $data
-            ));
-  if ($optional_headers !== null) {
-    $params['http']['header'] = $optional_headers;
-  }
-  $ctx = stream_context_create($params);
-  $fp = @fopen($url, 'rb', false, $ctx);
-  if (!$fp) {
-    throw new Exception("Problem with $url, $php_errormsg");
-  }
-  $response = @stream_get_contents($fp);
-  if ($response === false) {
-    throw new Exception("Problem reading data from $url, $php_errormsg");
-  }
-  return $response;
-}
-
 function is_weburl($s) {
     $parts = @parse_url( $s );
     if ( array_key_exists("scheme",$parts) ) {        
