f12294d7bd9602c577183433d31037aae76bc988
William Karageorgos <William.Karageorgos@cern.ch>
2016-10-24 15:14:49 +0300

    [SQL] proper SQL script after merging dev-home-report

--- sql/8.13.2-8.13.3.sql
+++ sql/8.13.2-8.13.3.sql
@@ -1,3 +1,28 @@
+/*
+ Copyright (C) 2015 IASA - Institute of Accelerating Systems and Applications (http://www.iasa.gr)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and 
+ limitations under the License.
+*/
+
+/* 
+EGI AppDB incremental SQL script
+Previous version: 8.13.2
+New version: 8.13.3
+Author: wvkarag@lovecraft.priv.iasa.gr
+*/
+
+START TRANSACTION;
+
 CREATE OR REPLACE FUNCTION public.ppl_vo_xml_report(
     mid integer,
     lim bigint DEFAULT 9223372036854775807::bigint,
@@ -261,3 +286,10 @@ $BODY$
   ROWS 1000;
 ALTER FUNCTION public.app_xml_report_counts(integer)
   OWNER TO appdb;
+
+
+INSERT INTO version (major,minor,revision,notes) 
+	SELECT 8, 13, 3, E'Functional XML support for researcher app/vo reports'
+	WHERE NOT EXISTS (SELECT * FROM version WHERE major=8 AND minor=13 AND revision=3);
+
+COMMIT	
