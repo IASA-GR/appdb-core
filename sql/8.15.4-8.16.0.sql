9fc11c0c302ffebcb2aa828b1619ed2b2aa012d6
William Karageorgos <wvkarageorgos@iasa.gr>
2017-10-19 18:14:20 +0300

    [SQL] Add base_mp_uri and strict_base_mp_uri to va_providers_to_xml_ext function

--- sql/8.15.4-8.16.0.sql
+++ sql/8.15.4-8.16.0.sql
@@ -820,8 +820,186 @@ CREATE TRIGGER tr_gocdb_sites_01_create_uuid
     FOR EACH ROW
     EXECUTE PROCEDURE gocdb.trfn_gocdb_sites_create_uuid();
 
+-- Function: public.va_provider_to_xml_ext(text)
+
+-- DROP FUNCTION public.va_provider_to_xml_ext(text);
+
+CREATE OR REPLACE FUNCTION public.va_provider_to_xml_ext(mid text)
+  RETURNS SETOF xml AS
+$BODY$
+BEGIN
+RETURN QUERY
+SELECT 
+	xmlelement(
+		name "virtualization:provider", 
+		xmlattributes(
+			va_providers.id,
+			beta,
+			in_production,
+			node_monitored,
+			service_downtime::int AS service_downtime,
+			service_status AS service_status,
+			service_status_date AS service_status_date
+		),
+		xmlelement(name "provider:name", sitename),
+		CASE WHEN NOT sites.id IS NULL THEN
+		XMLELEMENT(
+			name "appdb:site", 
+			XMLATTRIBUTES(
+				sites.id as id,
+				sites.name as name,
+				sites.productioninfrastructure as infrastructure, 
+				sites.certificationstatus as status,
+				sites.deleted as deleted,
+				sites.datasource as source
+			),
+			XMLELEMENT(
+				name "site:officialname", 
+				sites.officialname
+			), 
+			XMLELEMENT(
+				name "site:url", 
+				XMLATTRIBUTES('portal' as type),
+				sites.portalurl
+			), 
+			XMLELEMENT(
+				name "site:url", 
+				XMLATTRIBUTES('home' as type), 
+				sites.homeurl
+			)
+		)
+		END,
+		xmlelement(name "provider:url", url),
+		CASE WHEN EXISTS (SELECT * FROM va_provider_endpoints WHERE va_provider_endpoints.va_provider_id = va_providers.id) THEN 
+			array_to_string(array_agg(DISTINCT
+				xmlelement(name "provider:endpoint_url", endpoint_url)::text ||
+				xmlelement(name "provider:deployment_type", deployment_type)::text
+			),'')::xml 
+		END,
+		xmlelement(name "provider:gocdb_url", gocdb_url),
+		CASE WHEN COALESCE(host_dn, '') <> '' THEN xmlelement(name "provider:dn", host_dn) END,
+		CASE WHEN COALESCE(host_ip, '') <> '' THEN xmlelement(name "provider:ip", host_ip) END,
+		CASE WHEN COALESCE(host_os_id, 0) <> 0 THEN xmlelement(
+			name "provider:os", 
+			xmlattributes(host_os_id AS id),
+			oses.name
+		) END,
+		CASE WHEN COALESCE(host_arch_id, 0) <> 0 THEN xmlelement(
+			name "provider:arch", 
+			xmlattributes(host_arch_id AS id),
+			archs.name
+		) END,
+		country_to_xml(country_id),
+		CASE WHEN EXISTS(SELECT * FROM va_provider_templates WHERE va_provider_templates.va_provider_id = va_providers.id) THEN
+		array_to_string(array_agg(DISTINCT
+			xmlelement(name "provider:template",
+				 xmlattributes(
+					va_provider_templates.group_hash AS group_hash
+				 ),
+				 xmlelement(name "provider_template:resource_name", resource_name),
+				 xmlelement(name "provider_template:main_memory_size", memsize),
+				 xmlelement(name "provider_template:logical_cpus", logical_cpus),
+				 xmlelement(name "provider_template:physical_cpus", physical_cpus),
+				 xmlelement(name "provider_template:cpu_multiplicity", cpu_multiplicity),
+				 xmlelement(name "provider_template:resource_manager", resource_manager),
+				 xmlelement(name "provider_template:computing_manager", computing_manager),
+				 xmlelement(name "provider_template:os_family", os_family),
+				 xmlelement(name "provider_template:connectivity_in", connectivity_in),
+				 xmlelement(name "provider_template:connectivity_out", connectivity_out),
+				 xmlelement(name "provider_template:cpu_model", cpu_model),
+				 xmlelement(name "provider_template:disc_size", disc_size),
+				 xmlelement(name "provider_template:resource_id", resource_id)
+			)::text
+		), '')::xml
+		END,
+		CASE WHEN EXISTS(SELECT * FROM va_provider_images WHERE va_provider_images.va_provider_id = va_providers.id) THEN
+		(
+			SELECT (array_to_string(array_agg(DISTINCT
+				xmlelement(name "provider:image",
+					xmlattributes(
+						content_type,
+						mp_uri,
+						'https://' || COALESCE((SELECT data FROM config WHERE var = 'ui-host'), 'appdb.egi.eu') || '/store/vm/image/' || vmiinstances.guid::text || ':' || va_provider_images.vmiinstanceid::text AS  base_mp_uri,
+						'https://' || COALESCE((SELECT data FROM config WHERE var = 'ui-host'), 'appdb.egi.eu') || '/store/vm/image/' || vmiinstances.guid::text || ':' || va_provider_images.good_vmiinstanceid::text AS  strict_base_mp_uri,
+						vmiinstances.version AS "vmiversion",
+						va_provider_image_id,
+						va_provider_images.vmiinstanceid,
+						va_provider_images.vowide_vmiinstanceid,	
+						va_provider_images.good_vmiinstanceid,
+						applications.id as "appid", 
+						applications.name as "appname", 
+						applications.cname as "appcname", 
+						vos.id as "void", 
+						vos.name as "voname",
+						vapp_versions.archived
+					)
+				)::text), '')::XML
+			) FROM va_provider_images 
+			INNER JOIN vmiinstances ON vmiinstances.id = va_provider_images.vmiinstanceid
+			INNER JOIN vmiflavours ON vmiflavours.id = vmiinstances.vmiflavourid
+			INNER JOIN vmis ON vmis.id = vmiflavours.vmiid
+			INNER JOIN vapplications ON vapplications.id = vmis.vappid
+			INNER JOIN vapplists ON vapplists.vmiinstanceid = va_provider_images.vmiinstanceid
+			INNER JOIN vapp_versions ON vapp_versions.id = vapplists.vappversionid
+			INNER JOIN applications ON applications.id = vapplications.appid
+			LEFT OUTER JOIN vowide_image_list_images ON vowide_image_list_images.id = va_provider_images.vowide_vmiinstanceid
+			LEFT OUTER JOIN vowide_image_lists ON vowide_image_lists.id = vowide_image_list_images.vowide_image_list_id
+			LEFT OUTER JOIN vos ON vos.id = vowide_image_lists.void			
+			WHERE va_provider_id = va_providers.id AND ((
+				vowide_image_lists.state IN ('published'::e_vowide_image_state, 'obsolete'::e_vowide_image_state)
+			) OR (
+				vowide_image_lists.state IS NULL
+			)) 
+			
+		)
+		END
+	)
+FROM
+	va_providers 
+	LEFT JOIN oses ON oses.id = host_os_id
+	LEFT JOIN archs ON archs.id = host_arch_id
+	LEFT JOIN va_provider_endpoints ON va_provider_endpoints.va_provider_id = va_providers.id
+	LEFT JOIN va_provider_templates ON va_provider_templates.va_provider_id = va_providers.id
+	LEFT OUTER JOIN sites ON sites.name = va_providers.sitename
+WHERE va_providers.id = mid
+	GROUP BY 
+		va_providers.id,
+		va_providers.beta,
+		va_providers.in_production,
+		va_providers.node_monitored,
+		va_providers.service_downtime,
+		va_providers.service_status,
+		va_providers.service_status_date,
+		va_providers.sitename,
+		va_providers.url,
+		va_providers.gocdb_url,
+		va_providers.host_dn,
+		va_providers.host_ip,
+		va_providers.host_os_id,
+		va_providers.host_arch_id,
+		oses.name,
+		archs.name,
+		country_id,
+		sites.id,
+		sites.name,
+		sites.productioninfrastructure,
+		sites.certificationstatus,
+		sites.deleted,
+		sites.datasource,
+		sites.officialname,
+		sites.portalurl,
+		sites.homeurl
+;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION public.va_provider_to_xml_ext(text)
+  OWNER TO appdb;
+	
 INSERT INTO version (major,minor,revision,notes)
-       SELECT 8, 16, 0, E'Refactor va providers, based on new JSON information system data'
+       SELECT 8, 16, 0, E'Refactor va providers, based on new JSON information system data. Add base_mp_uri and strict_base_mp_uri to va_providers_to_xml_ext function'
        WHERE NOT EXISTS (SELECT * FROM version WHERE major=8 AND minor=16 AND revision=0);
 
 COMMIT;
