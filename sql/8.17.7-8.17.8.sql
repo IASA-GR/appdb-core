3d65fcb17c512ed203ffd52e1478b3d649fb106e
William Karageorgos <wvkarageorgos@iasa.gr>
2018-05-02 17:17:58 +0300

    [SQL] Make permissions refresh asynchronously for gocdb.site_contacts modifications

---
+++ sql/8.17.7-8.17.8.sql
@@ -0,0 +1,42 @@
+/*
+ Copyright (C) 2015 IASA - Institute of Accelerating Systems and Applications (http://www.iasa.gr)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and 
+ limitations under the License.
+*/
+
+/* 
+EGI AppDB incremental SQL script
+Previous version: 8.17.7
+New version: 8.17.8
+Author: wvkarag@lovecraft.priv.iasa.gr
+*/
+
+START TRANSACTION;
+
+CREATE OR REPLACE FUNCTION public.trfn_gocdb_site_contacts_refresh_permissions()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+        IF TG_WHEN = 'AFTER' THEN
+                PERFORM request_permissions_refresh();
+                RETURN NULL;
+        END IF;
+END;
+$function$;
+
+INSERT INTO version (major,minor,revision,notes) 
+	SELECT 8, 17, 8, E'Make permissions refresh asynchronously for gocdb.site_contacts modifications'
+	WHERE NOT EXISTS (SELECT * FROM version WHERE major=8 AND minor=17 AND revision=8);
+
+COMMIT;	
