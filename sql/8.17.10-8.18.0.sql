58f5f571f94897d48ffbd15270156e5c203e2c69
William Karageorgos <wvkarageorgos@iasa.gr>
2018-05-11 16:40:26 +0300

    [SQL] initial in-database support for OAI-PMH server functions

--- sql/8.17.10-8.18.0.sql
+++ sql/8.17.10-8.18.0.sql
@@ -1,3 +1,28 @@
+/*
+ Copyright (C) 2015 IASA - Institute of Accelerating Systems and Applications (http://www.iasa.gr)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and 
+ limitations under the License.
+*/
+
+/* 
+EGI AppDB incremental SQL script
+Previous version: 8.17.5
+New version: 8.17.6
+Author: nakos.al@iasa.gr
+*/
+
+START TRANSACTION;
+
 CREATE OR REPLACE FUNCTION hasparent(mid INT, mpid INT, mtbl TEXT) 
 RETURNS BOOLEAN AS
 $$
@@ -554,3 +579,9 @@ CREATE TRIGGER rtr_cache_appxmlcache_90_openaire
 BEFORE INSERT OR UPDATE ON cache.appxmlcache
 FOR EACH ROW
 EXECUTE PROCEDURE trfn_cache_appxmlcache_openaire();
+
+INSERT INTO version (major,minor,revision,notes) 
+        SELECT 8, 18, 0, E'Add initial support for OAI-PMH server functionality'
+        WHERE NOT EXISTS (SELECT * FROM version WHERE major=8 AND minor=17 AND revision=6);
+
+COMMIT;
