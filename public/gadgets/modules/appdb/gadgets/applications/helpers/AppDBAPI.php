c5c1ff9c3f405456eee90001399699b6aaa81ced
William Karageorgos <wvkarageorgos@iasa.gr>
2018-09-05 19:12:27 +0300

    [PHP] Gadgets module: use cURL instead of file_get_contents inside web_get_contents

--- public/gadgets/modules/appdb/gadgets/applications/helpers/AppDBAPI.php
+++ public/gadgets/modules/appdb/gadgets/applications/helpers/AppDBAPI.php
@@ -1,12 +1,17 @@
 <?php
 function web_get_contents($url) {
-	$arrContextOptions=array(
-		"ssl"=>array(
-			"verify_peer"=>false,
-			"verify_peer_name"=>false,
-		),
-	);   
-	return file_get_contents($url, false, stream_context_create($arrContextOptions));
+
+	$curl_handle=curl_init();
+	curl_setopt($curl_handle, CURLOPT_URL, $url);
+	curl_setopt($curl_handle, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl_handle, 181, 1 | 2);                                                                                                                                   
+	curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, false);                                                                                                                
+	curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);     
+	$query = curl_exec($curl_handle);
+	curl_close($curl_handle);
+	return $query;
 }
 
 class AppDBrestAPIHelper {
