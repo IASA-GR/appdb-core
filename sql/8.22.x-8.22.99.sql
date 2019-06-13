3f2485cd8ac3436babdf9fa4bf928dcd7e79bb24
William Karageorgos <wvkarageorgos@iasa.gr>
2019-06-13 16:09:12 +0300

    [SQL] added service_type attribute to production images XML in RESTful API

--- sql/8.22.x-8.22.99.sql
+++ sql/8.22.x-8.22.99.sql
@@ -1645,6 +1645,97 @@ LANGUAGE plpgsql;
 ALTER FUNCTION offers(sites) OWNER TO appdb;
 COMMENT ON FUNCTION offers(sites) IS 'Returns an array of supported cloud endpoints which offer VM images';
 
+CREATE OR REPLACE FUNCTION public.vapp_image_providers_to_xml(_appid integer)
+ RETURNS SETOF xml
+ LANGUAGE sql
+AS $function$
+ WITH hypervisors AS (
+	 WITH x AS (
+		 SELECT vmiflavours_2.id,
+		    unnest(vmiflavours_2.hypervisors) AS y
+		   FROM vmiflavours vmiflavours_2
+		)
+	 SELECT vmiflavours_1.id AS vmiflavourid,
+	    xmlagg(XMLELEMENT(NAME "virtualization:hypervisor", XMLATTRIBUTES(( SELECT hypervisors_1.id
+		   FROM public.hypervisors hypervisors_1
+		  WHERE hypervisors_1.name::text = x.y::text) AS id), x.y)) AS hypervisor
+	   FROM vmiflavours vmiflavours_1
+	JOIN x ON x.id = vmiflavours_1.id
+	GROUP BY vmiflavours_1.id
+)
+SELECT
+	xmlelement(
+		name "virtualization:image",
+		xmlattributes(
+			vaviews.vmiinstanceid,
+			va_provider_images.good_vmiinstanceid,
+			vaviews.vmiinstance_guid AS identifier,
+			vaviews.vmiinstance_version,
+			vaviews.va_version_archived AS archived,
+			vaviews.va_version_enabled AS enabled,
+			CASE WHEN vaviews.va_version_expireson >= NOW() THEN FALSE ELSE TRUE END AS isexpired
+		),
+		hypervisors.hypervisor::text::xml,
+--		XMLELEMENT(NAME "virtualization:hypervisors", array_to_string(vaviews.hypervisors, ',')::xml),
+		XMLELEMENT(NAME "virtualization:os", XMLATTRIBUTES(oses.id AS id, vaviews.osversion AS version, oses.os_family_id as family_id), oses.name),
+		XMLELEMENT(NAME "virtualization:arch", XMLATTRIBUTES(archs.id AS id), archs.name),
+		vmiinst_cntxscripts_to_xml(vaviews.vmiinstanceid),
+--		XMLELEMENT(NAME "virtualization:location", vaviews.uri),
+--		XMLELEMENT(NAME "virtualization:checksum", XMLATTRIBUTES(vaviews.checksumfunc AS checkfunc), vaviews.checksum),
+--		XMLELEMENT(NAME "virtualization:osversion", vaviews.osversion),
+		array_to_string(array_agg(DISTINCT
+			xmlelement(name "virtualization:provider",
+				xmlattributes(
+					va_provider_images.va_provider_id as provider_id,
+					va_provider_images.va_provider_image_id as occi_id, --FIXME: this should be renamed to endpoint_id
+					vowide_image_lists.void,
+					vos.name as voname,
+					va_provider_images.vmiinstanceid as vmiinstanceid,
+					va_providers.service_type
+				)
+			)::text
+		),'')::xml
+)
+FROM
+	applications
+	INNER JOIN vaviews ON vaviews.appid = applications.id
+	INNER JOIN va_provider_images AS va_provider_images ON va_provider_images.vmiinstanceid = vaviews.vmiinstanceid
+	INNER JOIN va_providers ON va_providers.id = va_provider_images.va_provider_id
+	LEFT OUTER JOIN hypervisors ON hypervisors.vmiflavourid = vaviews.vmiflavourid
+	LEFT OUTER JOIN archs ON archs.id = vaviews.archid
+	LEFT OUTER JOIN oses ON oses.id = vaviews.osid
+	LEFT OUTER JOIN vmiformats ON vmiformats.name::text = vaviews.format
+	LEFT OUTER JOIN app_vos ON app_vos.appid = applications.id
+	LEFT OUTER JOIN vowide_image_list_images ON vowide_image_list_images.id = va_provider_images.vowide_vmiinstanceid
+	LEFT OUTER JOIN vowide_image_lists ON vowide_image_lists.id = vowide_image_list_images.vowide_image_list_id AND (vowide_image_lists.state::text = 'published' OR vowide_image_lists.state::text = 'obsolete')
+	LEFT OUTER JOIN vos ON vos.id = vowide_image_lists.void
+WHERE
+	-- vaviews.vmiinstanceid = va_provider_images.good_vmiinstanceid AND
+	vaviews.va_version_published AND
+--	NOT vaviews.va_version_archived AND
+	applications.id = $1
+GROUP BY
+	applications.id,
+	--vaviews.uri,
+	--vaviews.checksumfunc,
+	--vaviews.checksum,
+	vaviews.osversion,
+	--vaviews.hypervisors,
+	hypervisors.hypervisor::text,
+	--vaviews.va_id,
+	--vaviews.vappversionid,
+	vaviews.vmiinstanceid,
+	va_provider_images.good_vmiinstanceid,
+	vaviews.vmiflavourid,
+	vaviews.vmiinstance_guid,
+	vaviews.vmiinstance_version,
+	vaviews.va_version_archived,
+	vaviews.va_version_enabled,
+	vaviews.va_version_expireson,
+	archs.id,
+	oses.id,
+	vmiformats.id;
+$function$;
 
 INSERT INTO version (major,minor,revision,notes) 
 SELECT 8, 22, 99, E'Support for OpenStack native cloud endpoints (aka va_providers)'
