8481616a9af8c9fc51ea2a83a6d37e918a869774
Thanos Liakopoulos <tliako@iasa.gr>
2020-02-27 10:46:41 +0200

    Added minor fix for nagios probes

--- library/nagios.php
+++ library/nagios.php
@@ -1,42 +1,85 @@
 <?php
-
+/**
+ * Class Nagios sets states to application errors and
+ * creates a readable error file with current 
+ * error, state , message and timestamp.
+ */
 class Nagios {
-	//Deletes file and logs error if any
+	/**
+	 * clear function deletes previous error file
+	 * and logs any process errors
+	 *
+	 * @param [type] $test	//Application error. 
+	 * @return void
+	 */
 	public static function clear($test) {
 		@unlink(APPLICATION_PATH . "/../cache/nagios-" . $test);
 		$e = error_get_last();
 		if($e['message']!==''){
-			// An error occurred
 			error_log("[nagios::clear]". $e)
 		}
 	}
-	//Creates error file, sets the state and message of the error and logs error if any
+	/**
+	 * set function Creates error file, 
+	 * sets the state and message of the error,
+	 * documents it and logs any process errors
+	 *
+	 * @param [type] $test	//Application error.
+	 * @param [type] $state	//Application error state.
+	 * @param [type] $msg  	//Application error message.
+	 * @return void
+	 */
 	private static function set($test, $state, $msg) {
 		$f = @fopen(APPLICATION_PATH . "/../cache/nagios-" . $test, "w");
 		$e = error_get_last();
 		$date = date(DATE_ISO8601, strtotime(date('m/d/Y H:i:s', time())));
 		if($e['message']!==''){
-			// An error occurred
 			error_log("[nagios::set]". $e)
 		}
-		@fwrite($f, $state . "\n");
-		@fwrite($date . "\n");
-		@fwrite($f, $msg . "\n");
-		@fclose($f);
+		if ( $f !== false ) {
+			@fwrite($f, $state . "\n");
+	    	@fwrite($date . "\n");
+		    @fwrite($f, $msg . "\n");
+		    @fclose($f);
+		}
 	}
-	//OK error state setter 
+	/**
+	 * OK error state setter 
+	 *
+	 * @param [type] $test	//Application error.
+	 * @param [type] $msg	//Application error message.
+	 * @return void
+	 */
 	public static function ok($test, $msg) {
 		Nagios::set($test, "OK", $msg);
 	}
-	//WARNING error state setter 
+	/**
+	 * WARNING error state setter 
+	 *
+	 * @param [type] $test	//Application error.
+	 * @param [type] $msg	//Application error message.
+	 * @return void
+	 */
 	public static function warning($test, $msg) {
 		Nagios::set($test, "WARNING", $msg);
 	}
-	//CRITICAL error state setter 
+	/**
+	 * CRITICAL error state setter 
+	 *
+	 * @param [type] $test	//Application error.
+	 * @param [type] $msg	//Application error message.
+	 * @return void
+	 */
 	public static function critical($test, $msg) {
 		Nagios::set($test, "CRITICAL", $msg);
 	}
-	//UNKNOWN error state setter 
+	/**
+	 * UNKNOWN error state setter
+	 *
+	 * @param [type] $test	//Application error.
+	 * @param [type] $msg	//Application error message.
+	 * @return void
+	 */ 
 	public static function unknown($test, $msg) {
 		Nagios::set($test, "UNKNOWN", $msg);
 	}
